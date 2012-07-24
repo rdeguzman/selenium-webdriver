cfbc4f06e478abb4b4b51e7f452ea4d8495eb8b5
Aldis Berjoza <aldis@cubesystems.lv>
2012-07-24 16:27:43 +0300

    Remove localhost test

--- freebsd-test.rb
+++ freebsd-test.rb
@@ -2,7 +2,6 @@ require "bundler/setup"
 require "selenium-webdriver"
 
 driver = Selenium::WebDriver.for :firefox
-driver.navigate.to "http://127.0.0.1"
 
 driver.navigate.to "http://www.google.com"
 driver.close
